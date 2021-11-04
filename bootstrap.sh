558cdb8e66ceeb35736fdc87973a08488b78f4bc
Leonid <outofspace230@gmail.com>
2021-11-04 23:23:37 +0000

    Add files
    - Add alacritty.yml.
    - Separate vim and neovim. Vim is not broken any longer trying to load
      neovim plugins.
    - Modify .bashrc adding alias to dotdtop.
    - Disable autoupdate every dotdrop run.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
-git submodule update --init
+git submodule update --init --recursive
+git submodule update --remote dotdrop
 pip install --user -r dotdrop/requirements.txt
 ./dotdrop/bootstrap.sh
+sed -i 's/DOTDROP_AUTOUPDATE-yes/DOTDROP_AUTOUPDATE-no/' dotdrop.sh
